8e6e1a1fe2622572c865ae32afeabc2da32b4ac1
yamachan <z@rinco.jp>
2023-04-22 10:16:25 +0900

    Add ts, and a simple sample page.

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Welcome page</h1>
+        count: {{ count }}
+        <span @click="addCount">[+]</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0);
+function addCount() {
+    count.value++;
+}
+</script>
